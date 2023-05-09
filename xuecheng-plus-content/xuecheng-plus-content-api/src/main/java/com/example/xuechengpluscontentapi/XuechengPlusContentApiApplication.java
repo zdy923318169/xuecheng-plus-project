46787ca052f0ca4bb4b2cdd72967422bf22592c5
zhangsan <zhangsan@163.com>
2023-05-09 17:05:44 +0800

    c

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/example/xuechengpluscontentapi/XuechengPlusContentApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.xuechengpluscontentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengPlusContentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengPlusContentApiApplication.class, args);
+    }
+
+}
